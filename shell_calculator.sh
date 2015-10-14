7f86592baa9dcd81b9361247b9b54cfa158e5dde
Soonthorn Sathiansathitwong <soonthorn@usablelabs.org>
2015-10-14 03:15:16 -0400

    add calculator

---
+++ shell_calculator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+read -p "Please enter the first number:" answer1
+read -p "Please enter the second number:" answer2
+read -p "Please choose an operator (+ - * /)" operator
+if [ $operator = "+" ]; then
+	echo $answer1+$answer2 = $((answer1+answer2))
+elif [ $operator = "-" ]; then
+	echo $answer1-$answer2 =$((answer1-answer2))
+elif [ $operator = * ]; then
+	echo $answer1*$answer2 =$((answer1*answer2))
+elif [ $operator = "/" ]; then
+	echo $answer1/$answer2 =$((answer1/answer2))
+fi
