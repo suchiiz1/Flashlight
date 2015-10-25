430c26c15d84bfc1d9a7d71687a90d619fe600b8
Soonthorn Sathiansathitwong <soonthorn@hotmail.com>
2015-10-25 03:55:01 -0400

    edit code shellscript

--- shellscript/shell_calculator.sh
+++ shellscript/shell_calculator.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+sum=0
+i="y"
 
-
-
-read -p "Please enter the first number:" answer1
-read -p "Please enter the second number:" answer2
-read -p "Please choose an operator (+ - * /)" operator
-if [ $operator = "+" ]; then
-	echo $answer1+$answer2 = $((answer1+answer2))
-elif [ $operator = "-" ]; then
-        echo $answer1-$answer2 =$((answer1-answer2))
-elif [ $operator = * ]; then
-        echo $answer1*$answer2 =$((answer1*answer2))
-elif [ $operator = "/" ]; then
-	echo $answer1/$answer2 =$((answer1/answer2))
-fi
+echo "Enter the first number"
+read n1
+echo "Enter the second number"
+read n2
+while [ $i = "y" ]
+do
+	echo "Enter your operator(+ - * /)"
+	read ch
+	case $ch in
+		    '+')sum=`expr $n1 + $n2`
+			         echo "$n1 + $n2 ="$sum;;
+		    '-')sum=`expr $n1 - $n2`
+	                 	 echo "$n1 - $n2 = "$sum;;
+	            '*')sum=`expr $n1 \* $n2`
+	 		         echo "$n1 * $n2 = "$sum;;
+		    '/')sum=`expr $n1 / $n2`
+	 		         echo "$n1 / $n2 = "$sum;;
+		    *)echo "Invalid choice";;
+esac
+echo "Do u want to continue ?(y/n)"
+  read i
+  if [ $i != "y" ]
+    then
+        exit
+	fi
+done    
+									
